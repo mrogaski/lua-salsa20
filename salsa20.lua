9c08669c357f8d407e5da41de03283a2b79299da
Mark Rogaski <mrogaski@pobox.com>
2014-07-14 20:07:24 -0400

    Added rowround function and refactored unit tests.

--- salsa20.lua
+++ salsa20.lua
@@ -2,11 +2,33 @@ module("salsa20", package.seeall)
 
 require("bit32")
 
-function quarterround(y0, y1, y2, y3)
-    local z1 = bit32.bxor(y1, bit32.lrotate(y0 + y3, 7))
-    local z2 = bit32.bxor(y2, bit32.lrotate(z1 + y0, 9))
-    local z3 = bit32.bxor(y3, bit32.lrotate(z2 + z1, 13))
-    local z0 = bit32.bxor(y0, bit32.lrotate(z3 + z2, 18))
-    return z0, z1, z2, z3
+function quarterround(y)
+    local z = {}
+    z[2] = bit32.bxor(y[2], bit32.lrotate(y[1] + y[4], 7))
+    z[3] = bit32.bxor(y[3], bit32.lrotate(z[2] + y[1], 9))
+    z[4] = bit32.bxor(y[4], bit32.lrotate(z[3] + z[2], 13))
+    z[1] = bit32.bxor(y[1], bit32.lrotate(z[4] + z[3], 18))
+    return z
 end
 
+function rowround(y)
+    local function apply(f, y, z, slice)
+        local iv = {}
+        for i, j in ipairs(slice) do
+            iv[i] = y[j]
+        end
+        local ov = f(iv)
+        for i, j in ipairs(slice) do
+            z[j] = ov[i]
+        end
+    end
+
+    local z = {}
+
+    apply(salsa20.quarterround, y, z, {1, 2, 3, 4})
+    apply(salsa20.quarterround, y, z, {6, 7, 8, 5})
+    apply(salsa20.quarterround, y, z, {11, 12, 9, 10})
+    apply(salsa20.quarterround, y, z, {16, 13, 14, 15})
+
+    return z
+end
--- test/test.lua
+++ test/test.lua
@@ -1,57 +1,86 @@
 
 require "salsa20"
 
-function roundtest(desc, f, data)
-    local n = 0
+function test_tuple(desc, f, n, data)
+    local c = 0
     local pass = 0
     for i, v in ipairs(data) do
         local arg = v[1]
         local out = v[2]
-        n = n + 1
-        local r1, r2, r3, r4 = f(arg[1], arg[2], arg[3], arg[4])
-        if r1 == out[1] and r2 == out[2] and r3 == out[3] and r4 == out[4] then
+        c = c + 1
+        local res = f(arg)
+        local err = false
+        for i = 1, n do
+            if res[i] ~= out[i] then
+                err = true
+                break
+            end
+        end
+        if not err then
             pass = pass + 1
         end
     end
-    if pass == n then
-        print(string.format("% 32s => PASS: %d/%d", desc, pass, n))
+    if pass == c then
+        print(string.format("% 32s => PASS: %d/%d", desc, pass, c))
     else
-        print(string.format("% 32s => FAIL: %d/%d", desc, pass, n))
+        print(string.format("% 32s => FAIL: %d/%d", desc, pass, c))
     end
 end
 
 
 local data = {
     {
-        {0x00000000, 0x00000000, 0x00000000, 0x00000000},
-        {0x00000000, 0x00000000, 0x00000000, 0x00000000}
+        { 0x00000000, 0x00000000, 0x00000000, 0x00000000 },
+        { 0x00000000, 0x00000000, 0x00000000, 0x00000000 }
     },
     {
-        {0x00000001, 0x00000000, 0x00000000, 0x00000000}, 
-        {0x08008145, 0x00000080, 0x00010200, 0x20500000}
+        { 0x00000001, 0x00000000, 0x00000000, 0x00000000 }, 
+        { 0x08008145, 0x00000080, 0x00010200, 0x20500000 }
     },
     {
-        {0x00000000, 0x00000001, 0x00000000, 0x00000000},
-        {0x88000100, 0x00000001, 0x00000200, 0x00402000}
+        { 0x00000000, 0x00000001, 0x00000000, 0x00000000 },
+        { 0x88000100, 0x00000001, 0x00000200, 0x00402000 }
     },
     {
-        {0x00000000, 0x00000000, 0x00000001, 0x00000000},
-        {0x80040000, 0x00000000, 0x00000001, 0x00002000}
+        { 0x00000000, 0x00000000, 0x00000001, 0x00000000 },
+        { 0x80040000, 0x00000000, 0x00000001, 0x00002000 }
     },
     {
-        {0x00000000, 0x00000000, 0x00000000, 0x00000001},
-        {0x00048044, 0x00000080, 0x00010000, 0x20100001}
+        { 0x00000000, 0x00000000, 0x00000000, 0x00000001 },
+        { 0x00048044, 0x00000080, 0x00010000, 0x20100001 }
     },
     {
-        {0xe7e8c006, 0xc4f9417d, 0x6479b4b2, 0x68c67137},
-        {0xe876d72b, 0x9361dfd5, 0xf1460244, 0x948541a3}
+        { 0xe7e8c006, 0xc4f9417d, 0x6479b4b2, 0x68c67137 },
+        { 0xe876d72b, 0x9361dfd5, 0xf1460244, 0x948541a3 }
     },
     {
-        {0xd3917c5b, 0x55f1c407, 0x52a58a7a, 0x8f887a3b},
-        {0x3e2f308c, 0xd90a8f36, 0x6ab2a923, 0x2883524c}
+        { 0xd3917c5b, 0x55f1c407, 0x52a58a7a, 0x8f887a3b },
+        { 0x3e2f308c, 0xd90a8f36, 0x6ab2a923, 0x2883524c }
     },
 }
-roundtest("quarterround()", salsa20.quarterround, data)
+test_tuple("quarterround()", salsa20.quarterround, 4, data)
 
-
+data = {
+    {
+        { 0x00000001, 0x00000000, 0x00000000, 0x00000000,
+          0x00000001, 0x00000000, 0x00000000, 0x00000000,
+          0x00000001, 0x00000000, 0x00000000, 0x00000000,
+          0x00000001, 0x00000000, 0x00000000, 0x00000000 },
+        { 0x08008145, 0x00000080, 0x00010200, 0x20500000,
+          0x20100001, 0x00048044, 0x00000080, 0x00010000,
+          0x00000001, 0x00002000, 0x80040000, 0x00000000,
+          0x00000001, 0x00000200, 0x00402000, 0x88000100 }
+    },
+    {
+        { 0x08521bd6, 0x1fe88837, 0xbb2aa576, 0x3aa26365,
+          0xc54c6a5b, 0x2fc74c2f, 0x6dd39cc3, 0xda0a64f6,
+          0x90a2f23d, 0x067f95a6, 0x06b35f61, 0x41e4732e,
+          0xe859c100, 0xea4d84b7, 0x0f619bff, 0xbc6e965a },
+        { 0xa890d39d, 0x65d71596, 0xe9487daa, 0xc8ca6a86,
+          0x949d2192, 0x764b7754, 0xe408d9b9, 0x7a41b4d1,
+          0x3402e183, 0x3c3af432, 0x50669f96, 0xd89ef0a8,
+          0x0040ede5, 0xb545fbce, 0xd257ed4f, 0x1818882d }
+    },
+}
+test_tuple("rowround()", salsa20.rowround, 16, data)
 
