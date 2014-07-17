f30628f0f5d51445d4ffef458f58324059b77f5f
Mark Rogaski <mrogaski@pobox.com>
2014-07-16 21:48:58 -0400

    Added encrypt/decrypt functions. Added luadoc.

--- salsa20.lua
+++ salsa20.lua
@@ -1,4 +1,30 @@
-module("salsa20", package.seeall)
+--[[--------------------------------------------------------------------------
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Mark Rogaski
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+--]]--------------------------------------------------------------------------
+
+local salsa20 = {}
 
 require("bit32")
 
@@ -13,7 +39,21 @@ local function apply(f, y, z, slice)
     end
 end
 
-function quarterround(y)
+local function byte_inc(b)
+    local c = 1
+    for i = 1, 8 do
+        local new = (b[i] + c) % 0xFF
+        c = math.floor((b[i] + c) / 0xFF)
+        b[i] = new
+        if c == 0 then
+            break
+        end
+    end
+    return b
+end
+
+
+function salsa20.quarterround(y)
     assert(#y == 4)
     local z = {}
     z[2] = bit32.bxor(y[2], bit32.lrotate(y[1] + y[4], 7))
@@ -23,7 +63,7 @@ function quarterround(y)
     return z
 end
 
-function rowround(y)
+function salsa20.rowround(y)
     assert(#y == 16)
     local z = {}
     apply(salsa20.quarterround, y, z, {1, 2, 3, 4})
@@ -33,7 +73,7 @@ function rowround(y)
     return z
 end
 
-function columnround(y)
+function salsa20.columnround(y)
     assert(#y == 16)
     local z = {}
     apply(salsa20.quarterround, y, z, {1, 5, 9, 13})
@@ -43,17 +83,17 @@ function columnround(y)
     return z
 end
 
-function doubleround(y)
+function salsa20.doubleround(y)
     assert(#y == 16)
     return salsa20.rowround(salsa20.columnround(y))
 end
 
-function littleendian(b)
+function salsa20.littleendian(b)
     assert(#b == 4)
     return b[1] + 2^8 * b[2] + 2^16 * b[3] + 2^24 * b[4]
 end
 
-function littleendian_inv(x)
+function salsa20.littleendian_inv(x)
     local b = {}
     for i = 1, 4 do
         b[i] = bit32.band(bit32.rshift(x, (i - 1) * 8), 0xFF)
@@ -61,8 +101,10 @@ function littleendian_inv(x)
     return b
 end
 
-function hash(b)
+function salsa20.hash(b, rounds)
+    rounds = rounds or 20
     assert(#b == 64)
+    assert(rounds == 20 or rounds == 12 or rounds == 8)
     local x = {}
     for i = 1, 16 do
         local quad = {}
@@ -72,7 +114,7 @@ function hash(b)
         x[i] = salsa20.littleendian(quad)
     end
     local z = salsa20.doubleround(x)
-    for i = 1, 9 do
+    for i = 1, ((rounds / 2) - 1) do
         z = salsa20.doubleround(z)
     end
     local h = {}
@@ -85,11 +127,13 @@ function hash(b)
     return h
 end
 
-function expand(k, n)
+function salsa20.expand(k, n)
     assert(#k == 16 or #k == 32)
     assert(#n == 16)
+
     local c = { string.byte(string.format("expand %2d-byte k", #k), 1, 16) } 
     local x = {}
+
     for i =  1,  4 do table.insert(x, c[i]) end
     for i =  1, 16 do table.insert(x, k[i]) end
     for i =  5,  8 do table.insert(x, c[i]) end
@@ -101,7 +145,109 @@ function expand(k, n)
         for i = 17, 32 do table.insert(x, k[i]) end
     end
     for i = 13, 16 do table.insert(x, c[i]) end
+
     return x
 end
 
+--- Generate the i^th 64-byte block of the Salsa20_k(v) 2^70-byte sequence.
+-- @param k A 16-byte or 32-byte sequence representing the secret key.
+-- @param v An 8-byte sequence representing the nonce.
+-- @param i An 8-byte sequence representing the stream position of the 64-byte block.
+-- @param rounds Optional number of rounds. My be 20 (default), 12, or 8.
+-- @return A 64-byte output block.
+function salsa20.generate(k, v, i, rounds)
+    assert(#k == 16 or #k == 32)
+    assert(#v == 8)
+    assert(#i == 8)
+    local n = {}
+    for j = 1, 8 do
+        n[j]     = v[j]
+        n[j + 8] = i[j]
+    end
+    return salsa20.hash(salsa20.expand(k, n), rounds)
+end
+
+--- Encrypt a string.
+-- @param key A 16-octet or 32-octet bytestring.
+-- @param nonce An 8-octet bytestring nonce.
+-- @param plaintext The unencrypted message.
+-- @param rounds Optional number of rounds. My be 20 (default), 12, or 8.
+-- @return The encrypted message.
+function salsa20.encrypt(key, nonce, plaintext, rounds)
+    assert(type(key) == "string" and (key:len() == 16 or key:len() == 32))
+    assert(type(nonce) == "string" and nonce:len() == 8)
+    assert(type(plaintext) == "string")
+    
+    local k = { key:byte(1, #key) }
+    local v = { nonce:byte(1, #nonce) }
+    local i = { 0, 0, 0, 0, 0, 0, 0, 0 }
+    local store = {}
+    local ciphertext = ""
+    
+    for _, m in pairs({ plaintext:byte(1, #plaintext) }) do
+        if #store == 0 then
+            store = salsa20.generate(k, v, i, rounds)
+            i = byte_inc(i)
+        end
+        local r = table.remove(store, 1)
+        ciphertext = ciphertext .. string.char(bit32.bxor(m, r))
+    end
+    return ciphertext
+end
+
+--- Decrypt a string.
+-- @param key A 16-octet or 32-octet bytestring.
+-- @param nonce An 8-octet bytestring nonce.
+-- @param ciphertext The encrypted message.
+-- @param rounds Optional number of rounds. My be 20 (default), 12, or 8.
+-- @return The unencrypted message.
+function salsa20.decrypt(key, nonce, ciphertext, rounds)
+    -- The easiest thing I've done all week.
+    salsa20.encrypt(key, nonce, ciphertext, rounds)
+end
+
+--- Encrypt a table of strings.
+-- @param key A 16-octet or 32-octet bytestring.
+-- @param nonce An 8-octet bytestring nonce.
+-- @param plaintext The unencrypted messages.
+-- @param rounds Optional number of rounds. My be 20 (default), 12, or 8.
+-- @return The encrypted messages.
+function salsa20.encrypt_table(key, nonce, plaintext, rounds)
+    assert(type(key) == "string" and (key:len() == 16 or key:len() == 32))
+    assert(type(nonce) == "string" and nonce:len() == 8)
+    assert(type(plaintext) == "table")
+    
+    local k = { key:byte(1, #key) }
+    local v = { nonce:byte(1, #nonce) }
+    local i = { 0, 0, 0, 0, 0, 0, 0, 0 }
+    local store = {}
+    local ciphertext = {}
+    
+    for _, s in pairs(plaintext) do
+        local segment = ""
+        for _, m in pairs({ s:byte(1, #s) }) do
+            if #store == 0 then
+                store = salsa20.generate(k, v, i, rounds)
+                i = byte_inc(i)
+            end
+            local r = table.remove(store, 1)
+            segment = segment .. string.char(bit32.bxor(m, r))
+        end
+        table.insert(ciphertext, segment)
+    end
+    return ciphertext
+end
+
+--- Decrypt a string.
+-- @param key A 16-octet or 32-octet bytestring.
+-- @param nonce An 8-octet bytestring nonce.
+-- @param ciphertext The encrypted messages.
+-- @param rounds Optional number of rounds. My be 20 (default), 12, or 8.
+-- @return The unencrypted messages.
+function salsa20.decrypt_table(key, nonce, ciphertext, rounds)
+    -- The second easiest thing I've done all week.
+    salsa20.encrypt_table(key, nonce, ciphertext, rounds)
+end
+
+return salsa20
 
